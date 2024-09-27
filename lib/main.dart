4010dd62a0a9404db8b52d33520aeec06ea8bace
Ishu <tattooless.kid@gmail.com>
2024-09-27 16:35:41 +0530

    Add Firebase initialization and configuration files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async{
   runApp(const MyApp());
-  WidgetsFlutterBinding.ensureInitialized()
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(); 
 }
 
